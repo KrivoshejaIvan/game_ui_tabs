bab39407d28b7441618a9708647375102a43c4fc
KrivoshejaIvan <ranger3331@gmail.com>
2019-02-18 20:50:38 +0200

    pushFiles

---
+++ js/script.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.tabs-section').on('click', '.content-tab:not(.tab-active)', (function (e) {
+        $('.content-tab').removeClass('tab-active');
+        $(this).addClass('tab-active'); //Почему нельзя использовать e.target?
+        $('.content-section').removeClass('active').eq($(this).index()).addClass('active');//Почему нельзя использовать e.target?
+    }))
+});
